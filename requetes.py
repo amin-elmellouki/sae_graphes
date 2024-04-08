28434be5c6d5db1f35fdd55f60e24713b89a3c79
El Mellouki <amin.el-mellouki@etu.univ-orleans.fr>
2024-04-08 12:17:33 +0200

    ajout du corps des fonctions à implémenter

--- requetes.py
+++ requetes.py
@@ -1,16 +1,103 @@
 # Q1
 def json_vers_nx(chemin):
-# Q2
-def collaborateurs_communs(G,u,v):
-# Q3
-def collaborateurs_proches(G,u,k):
-def est_proche(G,u,v,k=1):
-def distance_naive(G,u,v):
-def distance(G,u,v):
-# Q4
-def centralite(G,u):
-def centre_hollywood(G):
-# Q5
-def eloignement_max(G:nx.Graph):
-# Bonus
-def centralite_groupe(G,S):
+    """_summary_
+
+    Args:
+        chemin (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return res
+# # Q2
+# def collaborateurs_communs(G,u,v):
+#     """_summary_
+
+#     Args:
+#         G (_type_): _description_
+#         u (_type_): _description_
+#         v (_type_): _description_
+
+#     Returns:
+#         _type_: _description_
+#     """
+#     return res
+# # Q3
+# def collaborateurs_proches(G,u,k):
+#     """_summary_
+
+#     Args:
+#         G (_type_): _description_
+#         u (_type_): _description_
+#         k (_type_): _description_
+
+#     Returns:
+#         _type_: _description_
+#     """
+#     return res
+
+# def est_proche(G,u,v,k=1):
+#     """_summary_
+
+#     Args:
+#         G (_type_): _description_
+#         u (_type_): _description_
+#         v (_type_): _description_
+#         k (int, optional): _description_. Defaults to 1.
+
+#     Returns:
+#         _type_: _description_
+#     """
+#     return res
+
+# def distance_naive(G,u,v):
+#     """_summary_
+
+#     Args:
+#         G (_type_): _description_
+#         u (_type_): _description_
+#         v (_type_): _description_
+
+#     Returns:
+#         _type_: _description_
+#     """
+#     return res
+
+# def distance(G,u,v):
+
+#     return res
+# # Q4
+# def centralite(G,u):
+#     """_summary_
+
+#     Args:
+#         G (_type_): _description_
+#         u (_type_): _description_
+
+#     Returns:
+#         _type_: _description_
+#     """
+#     return res
+
+# def centre_hollywood(G):
+#     """_summary_
+
+#     Args:
+#         G (_type_): _description_
+
+#     Returns:
+#         _type_: _description_
+#     """
+#     return res
+
+# # Q5
+# def eloignement_max(G:nx.Graph):
+#     """_summary_
+
+#     Args:
+#         G (nx.Graph): _description_
+
+#     Returns:
+#         _type_: _description_
+#     """
+#     return res

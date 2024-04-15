ac9cf3704a2adc02edd1b76dd27b6c2ca9739c30
El Mellouki <amin.el-mellouki@etu.univ-orleans.fr>
2024-04-15 11:50:08 +0200

    modification du fichier json + ajout des docstring pour chaque fonction

--- requetes.py
+++ requetes.py
@@ -1,103 +1,117 @@
+import networkx as nx
+
 # Q1
 def json_vers_nx(chemin):
-    """_summary_
+    """Convertit le jeu de données (au format json) représentant les collaborations en un graphe Networkx exploitable.
 
     Args:
-        chemin (_type_): _description_
+        chemin (str): Le chemin du fichier json.
 
     Returns:
-        _type_: _description_
+        nx.Graph: Le graphe Networkx représentant les collaborations.
     """
     return res
-# # Q2
-# def collaborateurs_communs(G,u,v):
-#     """_summary_
 
-#     Args:
-#         G (_type_): _description_
-#         u (_type_): _description_
-#         v (_type_): _description_
+# Q2
+def collaborateurs_communs(G, u, v):
+    """Retourne l'ensemble des acteurs qui ont collaboré avec les deux acteurs donnés.
 
-#     Returns:
-#         _type_: _description_
-#     """
-#     return res
-# # Q3
-# def collaborateurs_proches(G,u,k):
-#     """_summary_
+    Args:
+        G (nx.Graph): Le graphe Networkx représentant les collaborations.
+        u: Le premier acteur.
+        v: Le deuxième acteur.
 
-#     Args:
-#         G (_type_): _description_
-#         u (_type_): _description_
-#         k (_type_): _description_
+    Returns:
+        set: L'ensemble des acteurs en commun.
+    """
+    return res
 
-#     Returns:
-#         _type_: _description_
-#     """
-#     return res
+# Q3
+def collaborateurs_proches(G, u, k):
+    """Retourne la liste des acteurs qui se trouvent à une distance au plus k de l'acteur donné.
 
-# def est_proche(G,u,v,k=1):
-#     """_summary_
+    Args:
+        G (nx.Graph): Le graphe Networkx représentant les collaborations.
+        u: L'acteur en question.
+        k (int): La distance maximale.
 
-#     Args:
-#         G (_type_): _description_
-#         u (_type_): _description_
-#         v (_type_): _description_
-#         k (int, optional): _description_. Defaults to 1.
+    Returns:
+        list: La liste des acteurs à distance au plus k de l'acteur donné.
+    """
+    return res
 
-#     Returns:
-#         _type_: _description_
-#     """
-#     return res
+def est_proche(G, u, v, k=1):
+    """Vérifie si deux acteurs se trouvent à une distance au plus k l'un de l'autre dans le graphe.
 
-# def distance_naive(G,u,v):
-#     """_summary_
+    Args:
+        G (nx.Graph): Le graphe Networkx représentant les collaborations.
+        u: Le premier acteur.
+        v: Le deuxième acteur.
+        k (int, optional): La distance maximale. Par défaut, 1.
 
-#     Args:
-#         G (_type_): _description_
-#         u (_type_): _description_
-#         v (_type_): _description_
+    Returns:
+        bool: True si les acteurs se trouvent à une distance au plus k l'un de l'autre, False sinon.
+    """
+    return res
 
-#     Returns:
-#         _type_: _description_
-#     """
-#     return res
+def distance_naive(G, u, v):
+    """Calcule la distance la plus courte entre deux acteurs dans le graphe de manière naïve.
 
-# def distance(G,u,v):
+    Args:
+        G (nx.Graph): Le graphe Networkx représentant les collaborations.
+        u: Le premier acteur.
+        v: Le deuxième acteur.
 
-#     return res
-# # Q4
-# def centralite(G,u):
-#     """_summary_
+    Returns:
+        int: La distance entre les deux acteurs.
+    """
+    return res
 
-#     Args:
-#         G (_type_): _description_
-#         u (_type_): _description_
+def distance(G, u, v):
+    """Calcule la distance la plus courte entre deux acteurs dans le graphe.
 
-#     Returns:
-#         _type_: _description_
-#     """
-#     return res
+    Args:
+        G (nx.Graph): Le graphe Networkx représentant les collaborations.
+        u: Le premier acteur.
+        v: Le deuxième acteur.
 
-# def centre_hollywood(G):
-#     """_summary_
+    Returns:
+        int: La distance entre les deux acteurs.
+    """
+    return res
 
-#     Args:
-#         G (_type_): _description_
+# Q4
+def centralite(G, u):
+    """Calcule la centralité d'un acteur dans le graphe.
 
-#     Returns:
-#         _type_: _description_
-#     """
-#     return res
+    Args:
+        G (nx.Graph): Le graphe Networkx représentant les collaborations.
+        u: L'acteur dont on veut calculer la centralité.
 
-# # Q5
-# def eloignement_max(G:nx.Graph):
-#     """_summary_
+    Returns:
+        int: La centralité de l'acteur.
+    """
+    return res
 
-#     Args:
-#         G (nx.Graph): _description_
+def centre_hollywood(G):
+    """Détermine l'acteur le plus central dans le graphe.
 
-#     Returns:
-#         _type_: _description_
-#     """
-#     return res
+    Args:
+        G (nx.Graph): Le graphe Networkx représentant les collaborations.
+
+    Returns:
+        str: L'acteur le plus central.
+    """
+    return res
+
+# Q5
+def eloignement_max(G: nx.Graph):
+    """Détermine la distance maximale entre toutes les paires d'acteurs dans le graphe.
+
+    Args:
+        G (nx.Graph): Le graphe Networkx représentant les collaborations.
+
+    Returns:
+        int: La distance maximale entre toutes les paires d'acteurs.
+    """
+    return res

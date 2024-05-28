10cd6c514aa470dfb7d8f2244c3322f38ddae4d5
Fahed Bouamrani <fahed.bouamrani@etu.univ-orleans.fr>
2024-05-28 15:00:42 +0200

    Ajout d'un gitignore pour le pycache + ajout du fichier de test

---
+++ tests_requetes.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import requetes
+
+def test_collaborateurs_communs():
+    G = nx.Graph()
+    G.add_edge("Acteur A", "Acteur B")
+    G.add_edge("Acteur A", "Acteur C")
+    G.add_edge("Acteur B", "Acteur C")
+    G.add_edge("Acteur B", "Acteur D")
+    G.add_edge("Acteur C", "Acteur D")
+    G.add_edge("Acteur D", "Acteur E")
+
+    assert requetes.collaborateurs_communs(G, "Acteur A", "Acteur B") == {"Acteur C"}
+
+    assert requetes.collaborateurs_communs(G, "Acteur A", "Acteur E") == set()
+
+    G.add_node("Acteur F")
+    assert requetes.collaborateurs_communs(G, "Acteur A", "Acteur F") == set()
+
+    G.add_nodes_from(["Acteur G", "Acteur H"])
+    assert requetes.collaborateurs_communs(G, "Acteur G", "Acteur H") == set()
